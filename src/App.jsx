06ad1d5b78446079daf2ee3cc25c5fd536017966
Benjamin Maudslay <benjaminmaudslay@Benjamins-MacBook-Pro-1235.local>
2021-09-27 15:45:19 +0100

    setup fetch request & display data

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
-import './App.css';
+import { useState } from "react"
+import "./App.css";
 
 const App = () => {
-  const handleFetch = () => {
-    console.log("hiya")
-  }
+  const [data, setData] = useState({})
+
+  const handleFetch = async () => {
+    let response = await fetch(
+      "https://swquotesapi.digitaljedi.dk/api/SWQuote/RandomStarWarsQuote"
+    );
+    let data = await response.json();
+    setData(data)
+  };
 
   return (
     <div>
       <h1>Star Wars</h1>
       <button onClick={handleFetch}>handleFetch</button>
+      <p>{data.content}</p>
     </div>
-  )
-}
+  );
+};
 
 export default App;
