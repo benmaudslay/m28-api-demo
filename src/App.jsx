35c360b00d7cd7130b75ca919e30fb8d1b7f5df8
Benjamin Maudslay <benjaminmaudslay@Benjamins-MacBook-Pro-1235.local>
2021-09-27 16:11:08 +0100

    added use effect & class component comparison with lifecycle methods

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,49 @@
-import { useState } from "react"
+import React, { useState, useEffect } from "react"
 import "./App.css";
+
+// class App extends React.Component {
+//   state = {
+//     data: {}
+//   }
+
+//   handleFetch = async () => {
+//     let response = await fetch(
+//       "https://swquotesapi.digitaljedi.dk/api/SWQuote/RandomStarWarsQuote"
+//     );
+//     let data = await response.json();
+//     this.setState({ data: data })
+//   }
+
+//   componentDidMount() {
+//     this.handleFetch()
+//   }
+
+//   componentDidUpdate() {
+//     console.log("I just updated!")
+//   }
+
+//   render() {
+//     const { data } = this.state
+//     return (
+//       <div>
+//         <h1>Star Wars</h1>
+//         <button onClick={this.handleFetch}>handleFetch</button>
+//         <p>{data.content}</p>
+//       </div>
+//     )
+//   }
+// }
 
 const App = () => {
   const [data, setData] = useState({})
+
+  useEffect(() => {
+    handleFetch()
+  }, [])
+
+  useEffect(() => {
+    console.log("you changed the data")
+  }, [data])
 
   const handleFetch = async () => {
     let response = await fetch(
@@ -11,6 +52,7 @@ const App = () => {
     let data = await response.json();
     setData(data)
   };
+
 
   return (
     <div>
